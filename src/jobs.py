78c5e5297374ada6e89abbda336b32adefed41f4
joaokeuwe <joaov2872@hotmail.com>
2022-09-12 20:32:52 -0300

    feat: :zap: Adicionando requisito 1, lendo um arquivo CSV

--- src/jobs.py
+++ src/jobs.py
@@ -1,11 +1,13 @@
 from functools import lru_cache
 import csv
 
+
 @lru_cache
 def read(path):
-   newFile = []
-   with open(path) as file:
-      filecsv = csv.DictReader(file)
-      for data in filecsv:
-         newFile.append(data)
-   return newFile 
+
+    with open(path) as file:
+        filecsv = csv.DictReader(file, delimiter=",", quotechar='"')
+        newFile = []
+        for data in filecsv:
+            newFile.append(data)
+    return newFile
